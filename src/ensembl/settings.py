0780cd0dbf4b07520c489c6a750a1374e2ef6f8a
satya.adda <addasaiteja@gmail.com>
2023-06-04 23:57:54 +0530

    create django models for ncbi taxonomy database

--- src/ensembl/settings.py
+++ src/ensembl/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'taxon_search.apps.TaxonSearchConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/taxon_search/models.py
+++ src/taxon_search/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class NCBITaxaNode(models.Model):
+
+    taxon_id = models.IntegerField(primary_key=True)
+    parent_id = models.ForeignKey('self', nullable=False, db_index=True)
+    rank = models.CharField(max_length=32, nullable=False, db_index=True)
+    genbank_hidden_flag = models.SmallIntegerField(nullable=False, default=0)
+    left_index = models.IntegerField(nullable=False, default=0, db_index=True)
+    right_index = models.IntegerField(nullable=False, default=0, db_index=True)
+    root_id = models.IntegerField(nullable=False, default=1)
+
+    class Meta:
+        db_table = 'ncbi_taxa_node'
+
+
+
+class NCBITaxaName(models.Model):
+
+    taxon_id = models.ForeignKey(NCBITaxaNode, on_delete=models.CASCADE, primary_key=True, db_index=True)
+    name = models.CharField(max_length=500, db_index=True, primary_key=True)
+    name_class = models.CharField(50, nullable=False, db_index=True)
+
+    class Meta:
+        db_table = 'ncbi_taxa_name'
+
