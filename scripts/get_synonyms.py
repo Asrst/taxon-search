934bf5cd35e6411b17ea8e8e5ac8bc1de09fe2a4
satya.adda <addasaiteja@gmail.com>
2023-07-05 01:44:17 +0530

    include whole tree as synonyms for the species

--- scripts/get_synonyms.py
+++ scripts/get_synonyms.py
@@ -23,17 +23,29 @@ def get_taxon_names(taxon_ids, db_conn):
     query_df = pd.DataFrame()
     for i in range(len(taxon_ids[:])):
         taxonid = taxon_ids[i]
-        query = f"""SELECT *
-                FROM ncbi_taxa_name na
-                WHERE name_class in ('scientific name', 
-                'synonym', 'equivalent name')
-                AND taxon_id={taxonid}"""
+        query = f"""SELECT n2.* , na.name, na.name_class
+                    FROM ncbi_taxa_node n1 
+                    JOIN (ncbi_taxa_node n2
+                        LEFT JOIN ncbi_taxa_name na 
+                        ON n2.taxon_id = na.taxon_id)  
+                    ON n2.left_index <= n1.left_index 
+                    AND n2.right_index >= n1.right_index 
+                    WHERE n1.taxon_id = {taxonid}
+                    ORDER BY left_index"""
         df = pd.read_sql_query(query, db_conn)
+        df['query_taxon_id'] = taxonid
+
         query_df = pd.concat([query_df, df])
     
     query_df = query_df.drop_duplicates()
-    syn_df = (query_df.sort_values(by=['taxon_id', 'name_class'])
-              .groupby(["taxon_id"])['name'].apply(", ".join).reset_index())
+    # syn_df = (query_df.sort_values(by=['taxon_id', 'name_class'])
+    #           .groupby(["taxon_id"])['name'].apply(", ".join).reset_index())
+    
+    syn_df = query_df[(~query_df['rank'].isin(['no rank'])) & 
+                      (query_df['name_class'].isin(['scientific name', 'synonym', 'equivalent name']))]
+    
+    syn_df = (syn_df.sort_values(by=["query_taxon_id", "left_index"], ascending=False)
+                .groupby(["query_taxon_id"])['name'].apply(", ".join).reset_index())
 
     return syn_df
 
